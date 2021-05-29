ce45857e7e3b792c69190089bd3d2d19681abca8
BOUTAHIRI SOUFIANE <56368243+soufianebt@users.noreply.github.com>
2021-05-29 20:30:35 +0100

    fill pushing

---
+++ Assets/Scripts/ControleurJoueur.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class ControleurJoueur : MonoBehaviour
+{
+    public float vitesse;
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 mouvement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(mouvement * vitesse);
+    }
+
+void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cible"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
